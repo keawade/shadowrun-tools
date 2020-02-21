1ce19f82793db42df77bb9b59c2bca4dba29c826
Keith Wade <keawade@protonmail.com>
2020-02-20 22:00:19 -0600

    Change IAttributes and ISkills to types for better typing

--- src/lib/interfaces/IAttributes.ts
+++ src/lib/interfaces/IAttributes.ts
@@ -1,14 +1,5 @@
 import { Attribute } from '../enums/Attribute';
 
-export interface IAttributes {
-  [Attribute.Body]: number;
-  [Attribute.Agility]: number;
-  [Attribute.Reaction]: number;
-  [Attribute.Strength]: number;
-  [Attribute.Willpower]: number;
-  [Attribute.Logic]: number;
-  [Attribute.Intuition]: number;
-  [Attribute.Charisma]: number;
-  [Attribute.Edge]: number;
-  [Attribute.Magic]: number;
-}
+export type IAttributes = {
+  [A in Attribute]: number;
+};
