49e849dada864ea670d11a2e30f5c147dbda19ba
Keith Wade <keawade@protonmail.com>
2020-02-20 18:31:03 -0600

    Update roll to use pool size

--- src/functions/roll.ts
+++ src/functions/roll.ts
@@ -1,6 +1,6 @@
-export const roll = (dice: number) => {
+export const roll = (poolSize: number) => {
   let result = 0;
-  for (let i = 0; i < dice; i++) {
+  for (let i = 0; i < poolSize; i++) {
     result += Math.floor(Math.random() * 6);
   }
   return result;
