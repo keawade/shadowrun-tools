c0c87c6d62764010bdb0b5508fd5ccd2faf9ee8b
Keith Wade <keawade@protonmail.com>
2020-02-22 10:42:58 -0600

    Augmented UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'aug-attr-spliced';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='logo-augmented-ui'>This is the aug</div>
+
+      <div className='my-augfiguration'>
+        <input></input>
+      </div>
+      <div>
+        <input></input>
+      </div>
+      <div>
+        <input></input>
+      </div>
+      <div>
+        <input></input>
+      </div>
+      <div>
+        <input></input>
+      </div>
     </div>
   );
 }
