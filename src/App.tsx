ce65ef40ed388284e0afeaff3f650914a57cf0c5
Keith Wade <keawade@protonmail.com>
2020-02-22 11:47:23 -0600

    SASS, partial styling, fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import './App.css';
-import 'aug-attr-spliced';
+import './styles/app.scss';
 
 function App() {
   return (
-    <div>
-      <div className='logo-augmented-ui'>This is the aug</div>
+    <>
+      <header>Shadowrun Utils</header>
+      <p>
+        bicycle render-farm modem faded marketing nodal point apophenia garage
+        singularity disposable. disposable woman towards order-flow vinyl denim
+        warehouse man kanji voodoo god. franchise meta- drugs neon face forwards
+        woman disposable voodoo god assault post-. Chiba euro-pop shrine post-
+        dome camera franchise otaku nodal point futurity. San Francisco drugs
+        knife savant beef noodles order-flow post- geodesic narrative car.
+      </p>
+      <p>
+        refrigerator office -ware corrupted woman wonton soup network j-pop
+        futurity refrigerator. Legba 3D-printed wonton soup j-pop marketing
+        nano- sub-orbital Shibuya Tokyo apophenia. crypto- BASE jump city
+        tiger-team vehicle denim soul-delay smart- paranoid dome. digital
+        disposable Tokyo uplink spook network apophenia table realism city.
+        pistol tiger-team sub-orbital monofilament tanto drone -ware lights
+        realism grenade.
+      </p>
+      <p>
+        towards sign bridge fluidity Kowloon San Francisco fetishism narrative
+        convenience store free-market. assassin disposable computer uplink Legba
+        pen corrupted shoes math- market. 8-bit -ware man corporation -space
+        tower meta- smart- euro-pop courier. tank-traps vinyl 3D-printed knife
+        vinyl systemic tiger-team sensory urban man. gang tube physical Chiba
+        Kowloon sensory refrigerator voodoo god garage nano-.
+      </p>
+      <p>
+        nodality meta- apophenia katana decay spook hacker geodesic chrome
+        geodesic. face forwards 3D-printed human disposable systemic franchise
+        assault -space sprawl numinous. garage drone bicycle sensory rebar
+        plastic voodoo god alcohol paranoid media. grenade tank-traps RAF
+        sensory faded refrigerator Kowloon corporation realism advert. rain
+        lights spook assault sub-orbital woman vinyl towards artisanal pen.
+      </p>
 
-      <div className='my-augfiguration'>
-        <input></input>
-      </div>
       <div>
         <input></input>
       </div>
@@ -22,7 +51,10 @@ function App() {
       <div>
         <input></input>
       </div>
-    </div>
+      <div>
+        <input></input>
+      </div>
+    </>
   );
 }
 
---
+++ src/declarations.d.ts
@@ -0,0 +1,2 @@
+/** augmented-ui plugin: adds the augmented-ui attribute to html elements automatically from CSS */
+declare module 'aug-attr-spliced';
