5ee6a5dadf00ec63257b55bfbee92f2dc111e402
Keith Wade <keawade@protonmail.com>
2020-03-08 15:40:27 -0500

    Set up underlying selection state, style base on selection

--- src/components/PriorityTable.tsx
+++ src/components/PriorityTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { priorities } from '../lib/data/priorities';
 import Magic from './Magic';
@@ -6,6 +6,14 @@ import Metatype from './Metatype';
 import Nuyen from './Nuyen';
 
 const PriorityTable = () => {
+  const [selection, setSelection] = useState([
+    'metatype',
+    'attributes',
+    'skills',
+    'magic',
+    'resources',
+  ] as Array<'metatype' | 'attributes' | 'skills' | 'magic' | 'resources'>);
+
   return (
     <div className='container'>
       <div className='grid-header'></div>
@@ -14,21 +22,45 @@ const PriorityTable = () => {
       <div className='grid-header'>Skills</div>
       <div className='grid-header'>Magic/Resonance</div>
       <div className='grid-header'>Resources</div>
-      {priorities.map(priority => (
+      {priorities.map((priority, index) => (
         <>
           <div className='grid-header priority'>{priority.priority}</div>
-          <div className='grid-cell metatype'>
+          <div
+            className={`grid-cell metatype${
+              selection[index] === 'metatype' ? ' selected' : ''
+            }`}
+          >
             <Metatype
               metatype={priority.metatype}
               metatypeAdjustmentPoints={priority.adjustmentPoints}
             />
           </div>
-          <div className='grid-cell attribute'>{priority.attributes}</div>
-          <div className='grid-cell skill'>{priority.skills}</div>
-          <div className='grid-cell magic'>
+          <div
+            className={`grid-cell attribute${
+              selection[index] === 'attributes' ? ' selected' : ''
+            }`}
+          >
+            {priority.attributes}
+          </div>
+          <div
+            className={`grid-cell skills${
+              selection[index] === 'skills' ? ' selected' : ''
+            }`}
+          >
+            {priority.skills}
+          </div>
+          <div
+            className={`grid-cell magic${
+              selection[index] === 'magic' ? ' selected' : ''
+            }`}
+          >
             <Magic resonance={priority.magic} />
           </div>
-          <div className='grid-cell resource'>
+          <div
+            className={`grid-cell resources${
+              selection[index] === 'resources' ? ' selected' : ''
+            }`}
+          >
             <Nuyen amount={priority.resources} />
           </div>
         </>
