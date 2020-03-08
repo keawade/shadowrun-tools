0270531b38966bf0b8871d8399f79dbe25457fe5
Keith Wade <keawade@protonmail.com>
2020-03-08 14:05:49 -0500

    Clean up magic component

--- src/components/Magic.tsx
+++ src/components/Magic.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import { startCase } from 'lodash';
 
-import { MagicTypes, IMagic } from '../lib/interfaces/IPriority';
+import { IMagic } from '../lib/interfaces/IPriority';
 
 const Magic = ({ resonance }: { resonance: IMagic }) => {
-  const displayValues = (Object.keys(resonance) as MagicTypes[]).map(key => ({
-    key,
-    name: startCase(key),
-    value: resonance[key],
-  }));
-
   return (
     <>
-      {displayValues
-        .map(displayValue => {
-          switch (displayValue.key) {
-            case 'mundane':
-              return displayValue.name;
-            case 'technomancer':
-              return `${displayValue.name}: ${displayValue.value} Resonance`;
-            default:
-              return `${displayValue.name}: ${displayValue.value} Magic`;
-          }
-        })
-        .join(', ')}
+      {Object.keys(resonance).map(key => {
+        switch (key) {
+          case 'mundane':
+            return <div>{startCase(key)}</div>;
+          case 'technomancer':
+            return (
+              <div>
+                {startCase(key)}: {resonance[key]} Resonance
+              </div>
+            );
+          case 'fullMagician':
+          case 'aspectedMagician':
+          case 'mysticAdept':
+          case 'adept':
+            return (
+              <div>
+                {startCase(key)}: {resonance[key]} Magic
+              </div>
+            );
+          default:
+            // Should not be possible to hit as we have exhausted all data types
+            return null;
+        }
+      })}
     </>
   );
 };
